1058aa9bf57861804d1ccb8cc86f9d287ed1be97
Eugene Kang <kangeuge90@gmail.com>
2021-09-30 18:03:09 -0400

    First Commit, not pulling correct objects atm

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//  SOURCES: 
+//  Explains reddit api - https://www.reddit.com/dev/api/
+//  API - https://www.reddit.com/r/aww/.json
+
+const awwPosts = fetch('https://www.reddit.com/r/aww/best/.json')
+
+awwPosts
+// .then(response => {
+//     // first then
+//     // then means when the API finishes downloading
+//     // console.log(response);
+
+//     return response.json(); // converts into javascript object with the data requested
+// })
+    .then(data => {
+    // second then, after we get the javascript object
+    console.log(data);
+
+    const results = data.children[0];
+
+    for (result of results) {
+
+        // <div> <h2> film title </h2> </div>
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+
+        img.src = result.data.thumbnail; // how do I target OBJECT # 2, not Array piece # 2
+        div.append(img);
+
+        const postTitle = document.createElement('p');
+        postTitle.innerText = result.title;
+        div.append(postTitle);
+
+        document.querySelector('.main').append(div); // add div to the the end of our main
+
+    }
+})
